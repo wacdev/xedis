1cd2e99a2356d8b45eae5de323fdaa73bcbcd4c6
wacdev <i@wac.tax>
2023-05-11 12:33:15 +0800

    ♨

--- test/test.lua
+++ test/test.lua
@@ -2,6 +2,10 @@ local log = function(...)
   redis.log(redis.LOG_NOTICE, ...)
 end
 
+local HSET = function(key, field, val)
+  return redis.call("HSET", key, field, val)
+end
+
 function xpendclaim(keys, args)
   log("xxx", #keys, keys[1])
   return 1
